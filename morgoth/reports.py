0e1f621cc657253ababe7dd5cb923ef186c75e20
Tobias Preis <tobias.preis@uibk.ac.at>
2025-05-09 07:59:42 +0200

    check if trigdat loc finished

--- morgoth/reports.py
+++ morgoth/reports.py
@@ -3,6 +3,7 @@ import os
 
 from morgoth.utils.env import get_env_value
 from morgoth.upload import UploadReport, UploadAllPlots, UploadAllDataFiles
+from morgoth.downloaders import GatherTrigdatDownload
 
 base_dir = get_env_value("GBM_TRIGGER_DATA_DIR")
 
@@ -27,6 +28,7 @@ class CreateReportTTE(luigi.Task):
 
     def requires(self):
         return {
+            "trigdat_complete": FinishedTrigdat(grb_name=self.grb_name),
             "report": UploadReport(
                 grb_name=self.grb_name, report_type="tte", version=self.version
             ),
@@ -50,6 +52,10 @@ class CreateReportTrigdat(luigi.Task):
     resources = {"max_workers": 1}
     grb_name = luigi.Parameter()
     version = luigi.Parameter(default="v00")
+    if version == "v00":
+        priority = 100
+    else:
+        priority = 50
 
     def requires(self):
         return {
@@ -69,4 +75,27 @@ class CreateReportTrigdat(luigi.Task):
         return luigi.LocalTarget(os.path.join(base_dir, self.grb_name, filename))
 
     def run(self):
+        os.system(f"touch {self.output().path}")
+
+
+class FinishedTrigdat(luigi.Task):
+    resources = {"max_worker": 1}
+    priority = -100
+    grb_name = luigi.Parameter()
+
+    def requires(self):
+        return {"gather_trigdat": GatherTrigdatDownload(grb_name=self.grb_name)}
+
+    def output(self):
+        filename = "start_tte.txt"
+        return os.path.join(base_dir, self.grb_name, filename)
+
+    def run(self):
+        flag = True
+        while flag:
+            for v in ["v00", "v01", "v02"]:
+                filename = f"trigdat_{v}_report_done.txt"
+                if os.path.exists(os.path.join(base_dir, self.grb_name, filename)):
+                    flag = False
+                    break
         os.system(f"touch {self.output().path}")
